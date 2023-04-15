ca333bc59b4e030936d5dd9e8b9b28f92cbe58a0
Jefferson Abac <125109849+jefferson1fal@users.noreply.github.com>
2023-04-14 20:40:19 -0600

    Add files via upload

---
+++ ejercicios 2 de arreglos jefferson abac.cs
@@ -0,0 +1,63 @@
+﻿void arreglos()
+{
+    Console.Write("¿Cuántos alumnos tiene la clase?: ");
+    int numAlumnos = int.Parse(Console.ReadLine());
+
+    string[] nombres = new string[numAlumnos];
+    int[] notas = new int[numAlumnos];
+
+    for (int i = 0; i < numAlumnos; i++)
+    {
+        Console.Write($"\nNombre del alumno {i + 1}: ");
+        nombres[i] = Console.ReadLine();
+
+        Console.Write($"Nota del alumno {i + 1}: ");
+        notas[i] = int.Parse(Console.ReadLine());
+    }
+
+    int notaMasAlta = notas[0];
+    string nombreNotaMasAlta = nombres[0];
+
+    foreach (int nota in notas)
+    {
+        if (nota > notaMasAlta)
+        {
+            notaMasAlta = nota;
+        }
+    }
+
+    foreach (string nombre in nombres)
+    {
+        if (notas[Array.IndexOf(nombres, nombre)] == notaMasAlta)
+        {
+            nombreNotaMasAlta = nombre;
+            break;
+        }
+    }
+
+    int notaMasBaja = notas[0];
+    string nombreNotaMasBaja = nombres[0];
+
+    foreach (int nota in notas)
+    {
+        if (nota < notaMasBaja)
+        {
+            notaMasBaja = nota;
+        }
+    }
+
+    foreach (string nombre in nombres)
+    {
+        if (notas[Array.IndexOf(nombres, nombre)] == notaMasBaja)
+        {
+            nombreNotaMasBaja = nombre;
+            break;
+        }
+    }
+
+    Console.WriteLine($"La nota más alta es {notaMasAlta} y es del alumno {nombreNotaMasAlta}");
+    Console.WriteLine($"La nota más baja es {notaMasBaja} y es del alumno {nombreNotaMasBaja}");
+}
+
+arreglos();
+
